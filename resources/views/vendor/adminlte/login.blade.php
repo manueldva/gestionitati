73c94ebb4c1bbc3affac6e8026a2f2e67d3540b2
Manuel David Avila <manudva22@outlook.com>
2019-01-29 20:46:15 -0300

    cambios

--- resources/views/vendor/adminlte/login.blade.php
+++ resources/views/vendor/adminlte/login.blade.php
@@ -4,16 +4,26 @@
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/auth.css') }}">
     @yield('css')
+    <style>
+        body {overflow-y:hidden!important;}
+    </style>
 @stop
 
 @section('body_class', 'login-page')
 
 @section('body')
     <div class="login-box">
+        <div class="form-group"  align="center">
+            <img src="{!! asset('imagedefeult/navegador.jpeg') !!}" height="150" width="150" class="img-circle profile_img" alt="Computer Hope">
+        </div>
         <div class="login-logo">
-            <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</a>
+            <!--<a href="{{ url(config('adminlte.dashboard_url', 'home')) }}">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</a>-->
+            <h1>
+                Ingresar al Sistema
+            </h1>
         </div>
         <!-- /.login-logo -->
+        
         <div class="login-box-body">
             <p class="login-box-msg">{{ trans('adminlte::adminlte.login_message') }}</p>
             <form action="{{ url(config('adminlte.login_url', 'login')) }}" method="post">
@@ -32,6 +42,7 @@
                         </span>
                     @endif
                 </div>
+                
                 <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
                     <input type="password" name="password" class="form-control"
                            placeholder="{{ trans('adminlte::adminlte.password') }}">
@@ -42,12 +53,12 @@
                         </span>
                     @endif
                 </div>
-                <div class="row">
+                <div class="row" >
                     
                     <!-- /.col -->
-                    <div class="col-xs-4">
+                    <div  style="text-align: center"  >
                         <button type="submit"
-                                class="btn btn-primary btn-block btn-flat">{{ trans('adminlte::adminlte.sign_in') }}</button>
+                                class="btn btn btn-success"> <i class="fa fa-sign-in"></i>  {{ trans('adminlte::adminlte.sign_in') }} </button>
                     </div>
                     <!-- /.col -->
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 /*Route::get('/detallemistareasabiertas', 'HomeController@detallemistareasabiertas')->name('detallemistareasabiertas');
 Route::get('/detalleotrastareasabiertas', 'HomeController@detalleotrastareasabiertas')->name('detalleotrastareasabiertas');*/
 
