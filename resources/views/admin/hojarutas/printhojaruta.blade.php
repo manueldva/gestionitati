0388e11d94cf7d992f7dff744d8a5e22b79dd29e
produccion <produccion@produccion.com>
2019-07-13 10:42:32 -0300

    nuevo

--- resources/views/admin/hojarutas/printhojaruta.blade.php
+++ resources/views/admin/hojarutas/printhojaruta.blade.php
@@ -9,59 +9,150 @@
 <h3><center> Cod. Hoja Ruta: {{ $hojaruta->id }}  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Fecha:  {{ $hojaruta->fecha }}</h3>
 <h3><center>Vendedor:  {{ $hojaruta->empleado->empleado }}</h3>
 <h3><center> Cantidad de domicilios a visitar:  {{ $cantidad}}</h3>
-@if(count($cantidad_b) == 1)
-<h3><center> Barrio:  {{ $barrio}}</h3>
-@endif
+
 <div class="portlet-body">
-	<table id="clientes" class="table table-striped table-bordered table-advance table-hover table-responsive tablesorter">
-											
-		<thead>
-			<tr>
-	            <th>
-					<center>
-						<i></i>  Nro Cliente
-					</center>	
-				</th>
-				<th>
-					<center>
-						<i></i> Cliente
-					</center>	
-				</th>
-				<th>
-					<center>
-						<i></i> Dirección
-					</center>
-				</th>
-				<th>
-					<center>
-						<i></i> Celular
-					</center>
-				</th>
-				<th>
-					<center>
-						<i></i> Articulo
-					</center>
-				</th>
-				<th>
-					<center>
-						<i></i> Cant.
-					</center>
-				</th>
-				<th>
-					<center>
-						<i></i> Venta
-					</center>
-				</th>
-				<th>
-					<center>
-						<i></i> Observación
-					</center>
-				</th>
-			</tr>
-		</thead>
-		<tbody>
+	
 			@foreach ($hojarutas as $hojaruta)
-		
+				@if($tempid2 !== $hojaruta->barrio)
+									</tbody>
+						</table>				
+					</div>
+					<h3><center> Barrio:  {{ $hojaruta->barrio}} </h3>
+					
+						<table id="clientes" class="table table-striped table-bordered table-advance table-hover table-responsive tablesorter">
+																
+							<thead>
+								<tr>
+						            <th>
+										<center>
+											<i></i>  Nro CLIENTE
+										</center>	
+									</th>
+									<th>
+										<center>
+											<i></i> CLIENTE
+										</center>	
+									</th>
+									<th>
+										<center>
+											<i></i> DIRECCIÓN
+										</center>
+									</th>
+									<th>
+										<center>
+											<i></i> CELULAR
+										</center>
+									</th>
+									<th>
+										<center>
+											<i></i> ARTICULO
+										</center>
+									</th>
+									<th>
+										<center>
+											<i></i> CANT.
+										</center>
+									</th>
+									<th>
+										<center>
+											<i></i> VENTA
+										</center>
+									</th>
+									<th>
+										<center>
+											<i></i> OBSERVACIÓN
+										</center>
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+				                	<td >
+				                		@if($tempid !== $hojaruta->cliente_id)
+					                  		<center>
+					                  			<b>
+													{{ $hojaruta->cliente_id }}
+												</b>
+											</center>
+										@endif
+									</td>
+				                	<td>
+				                		@if($tempid !== $hojaruta->cliente_id)
+											<center>
+												<b>
+													{{ $hojaruta->cliente }}
+												</b>
+											</center>
+										@endif
+									</td>
+									<td >
+										@if($tempid !== $hojaruta->cliente_id)
+											<center>
+										
+												
+												@if($hojaruta->calle)
+													Calle {{ $hojaruta->calle }}
+												@endif
+												@if($hojaruta->numero)
+													Nro. {{ $hojaruta->numero }}
+												@endif
+												@if($hojaruta->manzana)
+													Mz. {{ $hojaruta->manzana }}
+												@endif
+												@if($hojaruta->casa)
+													C. {{ $hojaruta->casa }}
+												@endif
+												@if($hojaruta->seccion)
+													Seccion {{ $hojaruta->seccion }}
+												@endif
+												@if($hojaruta->lote)
+													Lote {{ $hojaruta->lote }}
+												@endif
+												@if($hojaruta->edificiotorre)
+													Edificio {{ $hojaruta->edificiotorre }}
+												@endif
+												@if($hojaruta->piso)
+													Piso/Dpto {{ $hojaruta->piso }}
+												@endif
+												@if($hojaruta->referenciadomicilio)
+													Ref. {{ $hojaruta->referenciadomicilio }}
+												@endif
+											
+											</center>
+										@endif
+									</td>
+									<td >
+										@if($tempid !== $hojaruta->cliente_id)
+											<center>
+												
+												{{ $hojaruta->celular }}
+											
+											</center>
+										@endif
+									</td>
+									<td >
+										<center>
+										{{ $hojaruta->articulo }}
+										</center>
+									</td>
+									<td>
+										<center>
+										{{ $hojaruta->cantidad }}
+										</center>
+									</td>
+									 <td>
+										
+									</td>
+				                    <td>
+										
+									</td>
+				                </tr>
+				                <?php 
+									$tempid = $hojaruta->cliente_id;
+									$tempid2 = $hojaruta->barrio;
+								?>
+				@else
+
                 <tr>
                 	<td >
                 		@if($tempid !== $hojaruta->cliente_id)
@@ -84,9 +175,7 @@
 					<td >
 						@if($tempid !== $hojaruta->cliente_id)
 							<center>
-								@if(count($cantidad_b) > 1)
-									B° {{ $hojaruta->barrio }}
-								@endif
+						
 								@if($hojaruta->calle)
 									Calle {{ $hojaruta->calle }}
 								@endif
@@ -109,19 +198,21 @@
 									Edificio {{ $hojaruta->edificiotorre }}
 								@endif
 								@if($hojaruta->piso)
-									Piso/Dpto {{ $hojaruta->piso }}
+										Piso/Dpto {{ $hojaruta->piso }}
 								@endif
 								@if($hojaruta->referenciadomicilio)
 									Ref. {{ $hojaruta->referenciadomicilio }}
 								@endif
-
+							
 							</center>
 						@endif
 					</td>
 					<td >
 						@if($tempid !== $hojaruta->cliente_id)
 							<center>
+								
 								{{ $hojaruta->celular }}
+							
 							</center>
 						@endif
 					</td>
@@ -144,7 +235,9 @@
                 </tr>
                 <?php 
 					$tempid = $hojaruta->cliente_id;
+					$tempid2 = $hojaruta->barrio;
 				?>
+			@endif
             @endforeach
 		</tbody>
 	</table>				
