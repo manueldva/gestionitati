dbb8e4a68563e880cc9c8a2ccc910ad53ac3c147
Manuel David Avila <manuel-avila@hacfsa.org.ar>
2019-10-21 10:43:21 -0300

    otros

---
+++ resources/views/admin/informes/informeclientessincomprarprint.blade.php
@@ -0,0 +1,129 @@
+@extends('layouts.report')
+
+@section('cuerpo')
+<h3><center>Informe Ventas en Oficina</h3>
+<h3><center>Datos procesados desde @if($fechadesde) {{ $fechadesde}} @endif Hasta  @if($fechahasta) {{ $fechahasta}} @endif</center></h3>
+
+<div class="row">
+	<div class="col-md-12">	
+
+	  <div class="box box-default">
+	  	<div class="box-header with-border">
+
+
+	      <h3 class="box-title">
+	      	
+	      </h3>
+	    </div>
+	    <!-- /.box-header -->
+	    <div class="box-body">
+			
+			<!-- /.col -->
+			<div class="col-md-6 pull-right">
+			  <!--<div class="box box-default">-->
+			    
+			    <div class="box-header with-border">
+			      
+
+			      <!--<h3 class="box-title">Datos procesados desde @if($fechadesde) {{ $fechadesde}} @endif Hasta  @if($fechahasta) {{ $fechahasta}} @endif</h3>-->
+			    </div>
+			    <!-- /.box-header -->
+			    <div class="box-body">
+			    	
+					<label>Articulos cargados:</label> 
+					<div class="form-group">
+					
+						<div class="form-group">
+							<div class="table-responsive">
+								<table id="table_general" class="table table-striped table-hover" data-form="Form">
+									<thead>
+										<tr>
+										<!--<th width="10px"> ID</th>-->
+											<th> Articulo</th>
+											<th> Precio U.</th>
+											<th> Cantidad</th>
+											<th> Subtotal</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($t_por_articulo as $total)
+											<tr>
+											 	<td>
+											 		{{ $total->articulo }}
+											 	</td>
+											 	<td>
+											 		{{ $total->precio }}
+											 	
+											 	</td>
+											 	<td>
+											 		{{ $total->cantidad }}
+											 	</td>
+											 	<td>
+											 		{{ $total->monto }}
+											 	</td>
+
+											</tr>
+										@endforeach
+										<tr>
+											<td>
+												<b>
+													Totales
+												</b>
+												
+											</td>
+											<td>
+												
+											</td>
+											<td>
+												<b>
+													{{ $cantidadgeneral }}
+												</b>
+											</td>
+											<td>
+												<b>
+													{{ $totalgeneral }}
+												</b>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br>
+				<br>
+				<div class="col-md-6 pull-right">
+					
+					<br>
+					<hr>
+					
+					<h3>
+						<div class="form-group">
+							<label>Efectivo total a recibir:  <b> {{ $totalgeneral }} </b> </label>
+
+						</div>
+					</h3>
+				</div>
+			<!-- aca agregar el div col-6 -->
+			
+			</div>
+	 	</div>
+	    <!-- /.box-body -->
+	  </div>
+	  <!-- /.box -->
+	</div>
+</div>
+
+</center>
+
+<script>
+
+
+
+
+</script>
+
+
+@endsection
+
